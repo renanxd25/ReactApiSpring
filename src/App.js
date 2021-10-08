b14286859569484e80782fadd551a536b6000a3f
Renan Mendes <renanxd7x@hotmail.com>
2021-10-08 03:17:52 -0300

    segundo commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom'
+import './App.css';
+import routesConfig from './routesConfig';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <Link to="/" >Home</Link>
+          <Link to="/user" >User</Link>
+        </div>
+        {routesConfig.map((value, key) => {
+          return <Route
+            key={key}
+            path={value.path}
+            component={value.component}
+            exact={value.exact}
+          ></Route>
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
